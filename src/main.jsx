64f7fdfea6f9b1b5e409582837607e1824f8cfbe
faztech3120-star <faztech3120@gmail.com>
2026-09-08 16:40:51 +0530

    Add toHex polyfills for mobile browser support
    Added polyfills for toHex method in Uint8Array, ArrayBuffer, and Array for better compatibility with mobile browsers.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,29 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Polyfill toHex for mobile browsers (Safari, Android Chrome, Samsung Internet)
+if (typeof Uint8Array.prototype.toHex !== 'function') {
+  Uint8Array.prototype.toHex = function () {
+    return Array.from(this)
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('');
+  };
+}
+
+if (typeof ArrayBuffer.prototype.toHex !== 'function') {
+  ArrayBuffer.prototype.toHex = function () {
+    return Array.from(new Uint8Array(this))
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('');
+  };
+}
+
+if (typeof Array.prototype.toHex !== 'function') {
+  Array.prototype.toHex = function () {
+    return this.map(b => (typeof b === 'number' ? b.toString(16).padStart(2, '0') : '')).join('');
+  };
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
